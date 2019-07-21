2377d771526da5291d3e4ccaa9491ff57bd6d6dd
DAVID BILLINGSLEY <dcb2124@gmail.com>
2019-07-21 23:20:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  
+  return document.querySelector('#nested .target') ;
+}
+
+function increaseRankBy(n){
+  
+  let lis = document.querySelectorAll('ul.ranked-list li')
+  
+  for (let rank in lis){
+    rank.innerHTML = (parseInt(rank.innerHTML)+=n).toString();
+  }
+  
+}
+
+function deepestChild(){
+  return 
+  
+}

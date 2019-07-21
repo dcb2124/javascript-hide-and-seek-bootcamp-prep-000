abbf5597f37d698cb6e4cc4c7410baf73247f8b3
DAVID BILLINGSLEY <dcb2124@gmail.com>
2019-07-21 23:35:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  
+  return document.querySelector('#nested .target') ;
+}
+
+function increaseRankBy(n){
+  
+  let lis = document.querySelectorAll('ul.ranked-list li')
+  
+  for (let i = 0; i < lis.length; i++) {
+    
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+    
+  }
+
+  
+}
+
+function deepestChild(){
+  
+  let node = document.querySelector('div#grand-node');
+  
+  while (node.querySelector('div')) {
+    node = node.querySelector('div');
+  }
+  
+  return node
+  
+}
